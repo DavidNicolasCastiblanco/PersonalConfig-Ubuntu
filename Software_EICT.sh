b1cf5a0b2a30f9b5d6849bd3e13032902f0ce0df
DavidNicolasCastiblanco <108084292+DavidNicolasCastiblanco@users.noreply.github.com>
2022-09-07 13:22:15 -0500

    Update Software_EICT.sh

--- Software_EICT.sh
+++ Software_EICT.sh
@@ -31,12 +31,12 @@ rm ./bat*.deb
 cd
 
 #Software Postgresql
-udo apt-get -y install postgresql
+sudo apt-get -y install postgresql
 
 #Software PgAdmin
 # Setup the repository
 # Install the public key for the repository (if not done previously):
-sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo
 # Create the repository configuration file:
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 # Install for both desktop and web modes:
